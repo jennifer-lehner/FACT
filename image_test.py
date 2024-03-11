cb0259f62019d8394d4c9fced18f819ba2c764c8
jennifer-lehner <jennifer-lehner@gmx.de>
2024-03-11 14:30:12 +0100

    Image before/after, in one plot, transform

--- image_test.py
+++ image_test.py
@@ -8,19 +8,22 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    train_data, train_label, test_data, test_label = return_dataset('mnist', scale=False)
+    train_data, train_label, test_data, test_label = return_dataset('syn', scale=False)
     S = {}
     S['imgs'] = train_data
     S['labels'] = train_label
 
-    image_before = train_data[0]
+    image_before = train_data[0]  #Hier Nummer von Bild in Datensatz ändern
     print(image_before.shape)
 
     transform = transforms.Compose([
         transforms.Resize(32),
         transforms.ToTensor(),
         # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        transforms.ColorJitter(brightness=0.4, contrast=0, saturation=0.2, hue=0.3),
+        transforms.RandomAdjustSharpness(sharpness_factor=2),
+        transforms.RandomInvert()
     ])
     dataset = Dataset(S['imgs'], S['labels'], transform=transform) #dataset: instance, Dataset: class
 
@@ -38,7 +41,7 @@ def main():
     #plt.show()  # Show the figure
 
 # Bild nach Transform:
-    image_after, label = dataset.__getitem__(0)
+    image_after, label = dataset.__getitem__(0)  #Hier Nummer von Bild in Datensatz ändern
     #print(image_after.shape)
     img2 = image_after.permute(1, 2, 0)
     img2 = img2.numpy()  # Convert the tensor to numpy for plotting
