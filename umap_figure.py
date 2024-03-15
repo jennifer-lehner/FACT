76b2f4dffa5206773685df9f8e436c924c835d89
jennifer-lehner <jennifer-lehner@gmx.de>
2024-03-15 12:22:14 +0100

    UMAP for one domain (mnist)

--- umap_figure.py
+++ umap_figure.py
@@ -1,45 +1,31 @@
 import torch
-from torch import nn
+import umap
 from datasets.dataset_read import digit_five_dataset_read
-from models.combined_models import CombinedModel
-from models.digit_model import Feature, Predictor
 import plotly.express as px
 from umap import UMAP
+import umap.plot
 
 def main():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     batch_size = 32
-    train_loader, test_loader = digit_five_dataset_read('mnist', 100, device, index_range=range(100),
+    train_loader, test_loader = digit_five_dataset_read('mnist', 100, device, index_range=range(20),
                                                         remove_digits=[1, 2, 3, 4, 5, 6, 7, 8, 9])
 
     mnist_images, _ = next(iter(train_loader))
+    mnist_images = mnist_images.reshape(20,3*32*32)
 
-    model = CombinedModel(Feature(), Predictor())
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-    loss_fn = nn.CrossEntropyLoss()
+    print(mnist_images.shape)
 
-    for epoch in range(10):
-        for data in train_loader:
-            #print(type(data))
-            x, y = data  # jeweils image input und label
-            y = y.long()  # convert to long
-            optimizer.zero_grad()  # set gradients to zero
-            y_pred = model(x)  # forward pass
-            loss = loss_fn(y_pred, y)  # compute loss
-            loss.backward()  # compute gradients
-            optimizer.step()  # update weights
-
-    umap_2d = UMAP(random_state=0)
+    umap_2d = UMAP(random_state=None)
     umap_2d.fit(mnist_images)
 
     projections = umap_2d.transform(mnist_images)
 
     fig = px.scatter(
         projections, x=0, y=1,
-        color=mnist_images.target.astype(str), labels={'color': 'digit'}
+        #color=mnist_images.target.astype(str), labels={'color': 'digit'}
     )
     fig.show()
-
 
 if __name__ == "__main__":
     main()
